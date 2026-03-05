b516ef023812072aeb9b0892503a336679636860
seanballug <20171143@s.ubaguio.edu>
2026-03-05 11:41:49 +0800

    Added products component with table, modal, and event binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/models/product.interface.ts
@@ -0,0 +1,15 @@
+export interface Product {
+
+  id: number;
+  name: string;
+  category: string;
+  price: number;
+  stock: number;
+  status: string;
+  description: string;
+
+  brand: string;
+  supplier: string;
+  rating: number;
+
+}
