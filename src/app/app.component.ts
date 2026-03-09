bcc6a90fa7b413f89434c49a573d02d9677fdfa7
seanballug <20171143@s.ubaguio.edu>
2026-03-09 08:29:09 +0800

    Added home component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ProductsComponent } from './products/products.component';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductsComponent],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'midtermProject';
-}
+export class AppComponent {}
